5dda37def33b6de01e508bc6a738af2a6c4c142c
nagarjuna <nag.hasti8@gmail.com>
2024-03-18 11:44:17 +0530

    database coonected with env update

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
+import os
+
 import mysql.connector
 
 
 def get_database_connection():
     conn_params = {
-        "host": "localhost",
-        "database": "aptomorrow",
-        "user": "root",
-        "password": "aPTomorrow@3195"
+        "host": os.getenv("DATABASE_HOST"),
+        "database": os.getenv("DATABASE_NAME"),
+        "user": os.getenv("DATABASE_USER"),
+        "password": os.getenv("DATABASE_PASSWORD")
     }
     try:
         conn = mysql.connector.connect(**conn_params)
